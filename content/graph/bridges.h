278ec26ea68721add4b921837196616d957140f3
Kushal <kushal.balabhadruni@students.iiit.ac.in>
2025-12-29 14:24:35 +0530

    compressed bridges

--- content/graph/bridges.h
+++ content/graph/bridges.h
@@ -2,37 +2,39 @@
  * Author: Anonymous
  * Description: bridges
  */
-// arya bridges
-void findBridges_dfs(int u, int p, int &time, vector<vector<int>> &adj,
-                     vector<int> &disc, vector<int> &low,
-                     vector<pair<int, int>> &bridges) {
-  disc[u] = low[u] = time++;
-
-  for (int v : adj[u]) {
-    if (v == p)
+int n;                   // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+void dfs(int v, int p = -1) {
+  visited[v] = true;
+  tin[v] = low[v] = timer++;
+  int children = 0;
+  for (int to : adj[v]) {
+    if (to == p)
       continue;
-
-    if (disc[v] != -1) {
-      low[u] = min(low[u], disc[v]);
+    if (visited[to]) {
+      low[v] = min(low[v], tin[to]);
     } else {
-      findBridges_dfs(v, u, time, adj, disc, low, bridges);
-      low[u] = min(low[u], low[v]);
-      if (low[v] > disc[u]) {
-        bridges.push_back({u, v});
-      }
+      dfs(to, v);
+      low[v] = min(low[v], low[to]);
+      if (low[to] >= tin[v] && p != -1)
+        IS_CUTPOINT(v);
+      if (low[to]> tin[v]) IS_BRIDGE
+      ++children;
     }
   }
+  if (p == -1 && children > 1)
+    IS_CUTPOINT(v);
 }
-
-vector<pair<int, int>> findBridges(int n, vector<vector<int>> &adj) {
-  vector<int> disc(n, -1), low(n, -1);
-  vector<pair<int, int>> bridges;
-  int time = 0;
-
+void find_cutpoints() {
+  timer = 0;
+  visited.assign(n, false);
+  tin.assign(n, -1);
+  low.assign(n, -1);
   for (int i = 0; i < n; ++i) {
-    if (disc[i] == -1) {
-      findBridges_dfs(i, -1, time, adj, disc, low, bridges);
-    }
+    if (!visited[i])
+      dfs(i);
   }
-  return bridges;
 }
