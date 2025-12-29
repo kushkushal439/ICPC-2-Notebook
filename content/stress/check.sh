1b923c8aba429a35edcfaa4c44218d50fc0e03a1
Kushal <kushal.balabhadruni@students.iiit.ac.in>
2025-12-29 12:34:34 +0530

    added segtree with lowerboud

---
+++ content/stress/check.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# 1. Make in directory
+
+g++ gen.cpp -o gen -O2
+g++ brute.cpp -o brute -O2
+g++ sol.cpp -o sol -O2
+
+for ((t=1; ; t++)); do
+    echo "Test $t"
+    ./gen 50 > in/in${t}.txt
+    # cat in/in${t}.txt
+
+    ./brute < in/in${t}.txt > out1.txt
+    ./sol < in/in${t}.txt > out2.txt
+
+    # diff out1.txt out2.txt
+
+    if ! diff -q out1.txt out2.txt >/dev/null; then
+        echo "Mismatch found on test $t!"
+        cat in/in${t}.txt
+        diff out1.txt out2.txt
+        break
+    fi
+done
+
+# also for times, use this:
+# #ifndef ONLINE_JUDGE
+# clock_t tStart = clock();
+# #endif
+
+# void runtime() {
+# #ifndef ONLINE_JUDGE
+#     double elapsed =
+#         (double)(clock() - tStart) / CLOCKS_PER_SEC;
+
+#     fprintf(stderr,
+#             ">> Runtime: %.10fs\n",
+#             elapsed);
+# #endif
+# }
